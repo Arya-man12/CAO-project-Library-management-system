2cf8ab273c102c7392e7bef5759bebdb74741710
Arya-man12 <71289218+Arya-man12@users.noreply.github.com>
2023-10-28 02:16:22 +0530

    Add files via upload

--- projectcopy.py
+++ projectcopy.py
@@ -12,11 +12,12 @@ try:
  from cython.cimports.openmp import omp_set_dynamic
 except AttributeError:
        print("No such attribute")
+import datetime
+from datetime import datetime
 mycon=sqltor.connect(host="localhost",user="root",passwd="aryaman", database="test")
 root=tkinter.Tk()
-
 if mycon.is_connected():
-       print("successfully connected to library")
+       print("Successfully connected to library")
        root.title("Welcome")
        widgt1=tkinter.Label(root,text='Successfully connected to library')
        widgt1.pack()
@@ -25,8 +26,6 @@ cursor=mycon.cursor(buffered=True)
 tnow=0
 q=None
 print("*********************")
-print('Welcome to the library.enter your choice to continue')
-dict1={}
 def usersadd(name):
        add="INSERT INTO users(USERNAME) VALUES({})".format(name)
        cursor.execute(add)
@@ -40,15 +39,17 @@ def usersrem(name):
 def display():
           print("We have the following books in our library:")
           cursor.execute("select bookname from library")
-          data=cursor.fetchall()
-          for row in data:
-             print(row)
           c=cursor.rowcount
+          data=cursor.fetchall()
+          mycon.commit()
+          for row in data:
+               print(row)
           print("number of books in library",c)
           print("*********************")
 def display2():
-                cursor.execute("select bookname,username from borrow")
+                cursor.execute("select bookname,username,date1 from borrow")
                 data2=cursor.fetchall()
+                mycon.commit()
                 print(data2)
 def addbook():
 
@@ -65,66 +66,72 @@ def addbook():
 def rembook():
     
              a=input("enter name of book you want to remove in '' ")
-             bk=("DELETE FROM library where bookname={}").format(a)
-             cursor.execute(bk)
-             mycon.commit()
-             print("book removed")
+             cursor.execute("select bookname from library WHERE bookname={}").format(a)
+             chck3=cursor.rowcount
+             if(chck3!=0):
+              bk=("DELETE FROM library where bookname={}").format(a)
+              cursor.execute(bk)
+              mycon.commit()
+             else:
+              print("book removed")
 
-def returnbk(name):
+def returnbk():
+                    
                            b=input("Enter book to be returned in ''")
                            cursor.execute("select bookname from borrow WHERE bookname={}".format(b))
                            chck1=cursor.rowcount
                            if(chck1!=0):
+                            
+                              del1="select DATEDIFF(curdate(),(select date1 from borrow where bookname={}))".format(b);
+                              cursor.execute(del1)
+                              mycon.commit()
+                              sub=str(cursor.fetchone()[0])
+                              delay=int(sub)
+                              delay=abs(delay)
+                            
+                              if(delay>7):
+                                print("Number of days late is",delay-7)
+                                print("Book overdue fine is 50rs per day")
+                                print("Total fine",delay*50)
+
                               del1="DELETE FROM borrow WHERE bookname={}".format(b)
                               cursor.execute(del1)
+                              m=cursor.rowcount
                               mycon.commit()
                               rt="INSERT INTO library(bookname) VALUES({})".format(b)
                               cursor.execute(rt)
                               mycon.commit()
-                              rettime=time.time()
-                              time_delta=(rettime-dict1[name])
-                              print("book returned after time",time_delta,"seconds")
-                              delay_time=time_delta//30
-                              if(delay_time>1):
-                                     print("you have to pay a fine of ",(delay_time -1)*5,"rupees")
+                        
                            else:
                                print("book not found in borrow")
                            print("*********************")
+                        
 def borrow(name):
              b=input("enter the name of the book you want to borrow in ''")
              cursor.execute("select bookname from library WHERE bookname={}".format(b))
              chck1=cursor.rowcount
+             from datetime import date
+            
              if(chck1!=0):
-               trs="INSERT INTO borrow(bookname,username) VALUES({},{})".format(b,name)
+               trs="INSERT INTO borrow(bookname,username,date1) VALUES({},{},curdate())".format(b,name)
                cursor.execute(trs)
                mycon.commit()
                tnow=time.time()
-               dict1[name]=tnow
-               
                print("book has been borrowed")
+               print("Book should be returned within 7 days")
                rem="DELETE FROM library WHERE (bookname={})".format(b)
                cursor.execute(rem)
                mycon.commit()
              else:
                 print("No such book in library")
              print("*********************")
-
-try:
- user=input("Enter your username in ''")
-except sqltor.IntegrityError as err:
-                        print("Username already logged in")
-ck="INSERT INTO users(USERNAME) VALUES({})".format(user)
-cursor.execute(ck)
-mycon.commit()
-sel="select*from users"
-cursor.execute(sel)
-mycon.commit()
-n=cursor.rowcount
-num_threads = n
-omp_set_dynamic(n)
-list1=[]
-
+iter=0
 while(True):
+       omp_set_dynamic(1)
+       if(iter==0):
+           user='admin'
+           num_threads = 1
+           print("Initial user is admin ,sign in a new user if you wish to borrow and return books")
        print("Welcome to the library,enter you choice from the menu")
        print("1.Display books available")
        print("2.borrow books")
@@ -144,18 +151,22 @@ while(True):
        if (a==1):
          with Pool(num_threads) as p:
                     p=Process(target=display,args=())
-                    
+    
          display()
        if (a==2):
-            borrow(user)
+           if(user!='admin'):
+              borrow(user)
+           else:
+               print("User not signed in")
        if (a==3):
             with Pool(num_threads) as p:
                     p=Process(target=addbook,args=())
             addbook()
        if (a==4):
-           
-                returnbk(user)
-      
+              if(user!='admin'):
+                  returnbk()
+              else:
+                print("User not signed in")
        if(a==5):
                  with Pool(num_threads) as p:
                     r=Process(target=display2,args=())
@@ -167,20 +178,25 @@ while(True):
                     p=Process(target=rembook,args=())
            rembook()
        if(a==7):
-              name=input("Enter name of new user")
-              usersadd(name)
+              user=input("Enter name of new user")
+              usersadd(user)
               cursor.execute("select username from users")
               n=cursor.rowcount
               num_threads = n
-              print("Number of users ",num_threads)
+              print("Number of current users",num_threads)
        if(a==8):
                
-              name=input("Enter name of  user")
-              usersrem(name)
+              name=input("Enter name of user")
+              cursor.execute("select USERNAME FROM users WHERE username={}".format(name))
+              online=cursor.rowcount
+              if(online==0):
+                    print("User not logged in")
+              else:           
+               usersrem(name)
               cursor.execute("select username from users")
               n=cursor.rowcount
               num_threads = n
-              print("Number of users ",num_threads)
+              print("Number of current users ",num_threads)
 
        print("Press q to quit or press enter to continue using library")
        uc=input()
@@ -189,5 +205,6 @@ while(True):
                print("thank you for using this program")
                print("*********************")
                sys.exit()
-
+       iter+=1
+      
        continue    
